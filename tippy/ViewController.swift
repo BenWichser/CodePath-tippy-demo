2503b98c52c55f6f276f21da089fe366dcfac553
Ben Wichser <wichserpdx@gmail.com>
2020-12-08 23:03:29 -0800

    working bare bones version

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  tippy
 //
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func hideKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+    
+        // Get the bill amount
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.20]
+        // Calculate the tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        // Update the tip and total labels
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
 
-
+    
+    }
 }
 
